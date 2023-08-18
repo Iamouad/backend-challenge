5c2f7dd5e81d9b7deea087ffadbf83e98c9afae9
ubuntu-aws <ubuntu-aws@gmail.com>
2023-08-18 12:23:30 +0000

    update pipeline

--- config/db.js
+++ config/db.js
@@ -4,6 +4,9 @@ const config = require('config');
 const db = process.env.mongoURI;
 const connectDB = async () => {
     try {
+        console.log("db...."+ db)
+        if(! db)
+            db = config.get('mongoURI');
         await mongoose.connect(db, {useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
